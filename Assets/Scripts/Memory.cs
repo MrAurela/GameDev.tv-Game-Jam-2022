fa7a946cd10ff7770a2db38cf566a07890931a28
aurelam1 <miroaurela@gmail.com>
2022-05-30 23:35:27 +0300

    Sound files added. Esc to restart level.

--- Assets/Scripts/Memory.cs
+++ Assets/Scripts/Memory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Memory : MonoBehaviour
 {
@@ -36,7 +37,7 @@ public class Memory : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SetClone();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
         if (learn)
