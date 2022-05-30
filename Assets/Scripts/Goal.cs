91f625dd9c727887e2077db64835d72b1b18e64a
aurelam1 <miroaurela@gmail.com>
2022-05-31 01:07:47 +0300

    Pushing the last changes to the project.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,13 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] bool death = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
         if (player != null)
         {
-            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount);
-            FindObjectOfType<SoundManager>().PlayWin();
+           
+            if (!death)
+            {
+                FindObjectOfType<SoundManager>().PlayWin();
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount);
+            }
+            else
+            {
+                FindObjectOfType<SoundManager>().PlayDeath();
+                SceneManager.LoadScene((6));
+            }
         }
     }
 }
